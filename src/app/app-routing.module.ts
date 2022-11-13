e712c57bea202d443e59bb047260397a96fedcf0
James Ng'ang'a <james.ng'ang'a@student.moringaschool.com>
2022-11-13 23:51:18 +0300

    ft: hard code projects page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectComponent } from './definition/project/project.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'project', component: ProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
