f1195866a566a60da58acce01e1993e448e17bdb
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2022-11-26 22:52:11 +0300

    chore: streamline the httpo service

--- src/app/library/exchangerate/exchangerate.component.ts
+++ src/app/library/exchangerate/exchangerate.component.ts
@@ -4,70 +4,65 @@ import { CurrencyService } from 'src/app/service/currency.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
-
 @Component({
   selector: 'app-exchangerate',
   templateUrl: './exchangerate.component.html',
-  styleUrls: ['./exchangerate.component.css']
+  styleUrls: ['./exchangerate.component.css'],
 })
 export class ExchangerateComponent implements OnInit {
-
-  @ViewChild(MatPaginator, {static: true}) paginator !: MatPaginator;
-  
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
   ELEMENT_DATA = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
   ];
-  
-  constructor(private curr: CurrencyService, private http: HttpClient) { }
 
-  displayedColumns!:any
-  dataSource!:MatTableDataSource<Element>
-  temp!:any;
+  constructor(private curr: CurrencyService, private http: HttpClient) {}
 
-  finalCall(){
-  
-    this.curr.currencies.length === 0 ? this.curr.getAllData() : console.log(); 
+  displayedColumns!: any;
+  // dataSource!: MatTableDataSource<Element>;
+  dataSource!: any;
+  temp!: any;
+
+  finalCall() {
+    this.curr.currencies.length === 0 ? this.curr.getAllData() : console.log();
 
     setTimeout(() => {
       this.displayedColumns = this.curr.displayedColumns;
 
-      console.log(this.curr.displayedColumns)
-      this.dataSource = new MatTableDataSource<Element>(this.curr.rates)
-      this.dataSource.paginator = this.paginator
-      
-    }, 3500)
+      console.log(this.curr.exchange_rate_has);
+      // this.dataSource = new MatTableDataSource<Element>(this.curr.rates);
+      this.dataSource = this.curr.exchange_rate_has;
+      this.dataSource.paginator = this.paginator;
+
+      if (this.dataSource) {
+        console.log(this.dataSource.paginator);
+      }
+    }, 3500);
   }
 
-  alert(value: any){
-    this.temp = value.currencies
-    console.log(value.currencies)
+  alert(value: any) {
+    this.temp = value.currencies;
+    console.log(value.currencies);
     setTimeout(() => {
-      this.temp = undefined
-    }, 50000)
+      this.temp = undefined;
+    }, 50000);
   }
 
   ngOnInit(): void {
-
     // let element = this.displayedColumns[this.displayedColumns.findIndex(item => item === "currencies")]
 
     // this.displayedColumns.splice(this.displayedColumns.findIndex(item => item === "currencies"), 1)
 
     // this.displayedColumns.splice(0, 0, element)
-    this.finalCall()
-
+    this.finalCall();
   }
-
-  
-  
-
 }
--- src/app/service/currency.service.ts
+++ src/app/service/currency.service.ts
@@ -2,89 +2,115 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { forkJoin, Observable, shareReplay } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class CurrencyService {
-
   // rates = exchange_rate_table
 
-  displayedColumns!:any
-  rates:any =[]
-  
+  displayedColumns!: any;
+  rates: any = [];
 
-  currencies: any[] = []
+  currencies: any[] = [];
 
-  exchange_rate: any = []
+  exchange_rate: any = [];
 
-  exchange_rate_has: any = []
-  
-  exchange_rate_table: any  = []
+  exchange_rate_has: any = [];
 
-  constructor(private http: HttpClient){}
+  exchange_rate_table: any = [];
 
-  getCurrencies() : Observable<any>{
-      const currObservable = this.http.get("http://127.0.0.1:8000/api/currency_repositories/").pipe(shareReplay())
-      currObservable.subscribe(
-        (data: any) => {
-                  for (var i=0; i<data.length; i++)  
-                    this.currencies.push(<Response>data[i]);
-                })
+  libraryNames: string[] = ['Default', 'Normal'];
 
-      return currObservable
-    }
+  constructor(private http: HttpClient) {}
 
-  getExchangeLibrary() : Observable<any>{
-      const currObservable = this.http.get("http://127.0.0.1:8000/api/exchange_rate_libraries/").pipe(shareReplay())
-      currObservable.subscribe(
-        (data: any) => {
-                  for (var i=0; i<data.length; i++)  
-                    this.exchange_rate.push(<Response>data[i]);
-                })
+  getCurrencies(): Observable<any> {
+    const currObservable = this.http
+      .get('http://127.0.0.1:8000/api/currencyrepository/')
+      .pipe(shareReplay());
+    currObservable.subscribe((data: any) => {
+      for (var i = 0; i < data.length; i++) {
+        this.currencies.push(<Response>data[i]);
+      }
+    });
 
-      return currObservable
-    }
+    return currObservable;
+  }
 
-    getAllData(){
-      let currencies = this.getCurrencies()
-      let libraries = this.getExchangeLibrary()
+  getExchangeLibrary(): Observable<any> {
+    const currObservable = this.http
+      .get('http://127.0.0.1:8000/api/exchangeratelibrary/')
+      .pipe(shareReplay());
+    currObservable.subscribe((data: any) => {
+      for (var i = 0; i < data.length; i++)
+        this.exchange_rate.push(<Response>data[i]);
+    });
 
-      forkJoin([currencies, libraries]).subscribe(_ => {
-        this.currencies.forEach((element: any) => {
-          this.exchange_rate_has.push(this.exchange_rate.filter((item: any) => (item.currency === element.id)))
+    return currObservable;
+  }
+
+  getAllData() {
+    let currencies = this.getCurrencies();
+    // displaying the observable by subscring to it
+    // currencies.subscribe((e) => {
+    //   console.log(e);
+    // });
+    let libraries = this.getExchangeLibrary();
+    // displaying the observable by subscring to it
+    // currencies.subscribe((e) => {
+    //   console.log(e);
+    // });
+
+    forkJoin([currencies, libraries]).subscribe((_) => {
+      console.log('this runs');
+      this.libraryNames.forEach((name: string) => {
+        this.exchange_rate.forEach((item: any) => {
+          if (item.ExchangeRateLibraryName === name) {
+            this.exchange_rate_has.push(item);
+          }
         });
-        console.log(this.exchange_rate_has)
-        this.exchange_rate_has.forEach((element: any) => {
-          let first: any ={}
-          let obj1: any ={}
-          let obj!:any
-          element.forEach((item:any)=> {
-            console.log(this.currencies[item.currency]?.currency, "ok")
-          first['currencies'] = this.currencies[item.currency-1]?.currency
-          obj1[item.year] = item.rate
-          obj = Object.assign(obj1, first)
-        });
-        console.log(obj)
-        this.rates.push(obj)
       });
-  
-      const temp : any = []
-  
+
+      console.log(this.exchange_rate_has);
+
+      // this.currencies.forEach((element: any) => {
+
+      //   // this.exchange_rate.forEach((item: any) => {
+      //   //   if (item.id === element.id) {
+      //   //     this.exchange_rate_has.push(item);
+      //   //   }
+      //   // });
+      //   // this.exchange_rate_has.push(
+      //   //   // this.exchange_rate.filter((item: any) => item.currency === element.id)
+      //   // );
+      // });
+      // console.log(this.exchange_rate_has);
+      // console.log(this.exchange_rate)
+
+      this.exchange_rate_has.forEach((element: any) => {
+        let first: any = {};
+        let obj1: any = {};
+        let obj!: any;
+        element.forEach((item: any) => {
+          console.log(this.currencies[item.currency]?.currency, 'ok');
+          first['currencies'] = this.currencies[item.currency - 1]?.currency;
+          obj1[item.year] = item.rate;
+          obj = Object.assign(obj1, first);
+        });
+        console.log(obj);
+        this.rates.push(obj);
+      });
+
+      const temp: any = [];
+
       this.rates.forEach((item: any) => {
-        for(const key in item){
-          temp.push(key)
+        for (const key in item) {
+          temp.push(key);
         }
-      })
-  
-      this.displayedColumns = Array.from(new Set(temp)).sort().reverse()
+      });
+
+      this.displayedColumns = Array.from(new Set(temp)).sort().reverse();
 
       let sock = true;
-
-      })
-
-    }
-    
- 
+    });
+  }
 }
